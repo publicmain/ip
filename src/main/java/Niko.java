0668c56286e2248d791f9031166e8f04f1b5f180
publicmain <yaokexiang1107@163.com>
2024-08-22 20:14:10 +0800

    level 0 complete

---
+++ src/main/java/Niko.java
@@ -0,0 +1,14 @@
+public class Niko {
+
+    public static void main(String[] args) {
+        System.out.println("________________________________________");
+        System.out.println("Hello! I'm Niko");
+        System.out.println("What can I do for you?");
+        System.out.println("________________________________________");
+
+        // Add any additional logic here if needed
+
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("________________________________________");
+    }
+}
