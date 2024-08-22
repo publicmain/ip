52d9e473ce10d7344f4addb1daaa71e2cf6781b6
publicmain <yaokexiang1107@163.com>
2024-08-22 20:34:53 +0800

    Revert "level 4 complete"
    This reverts commit 2a94ae5ff2293a7d1f31c860d4eae03e3358a2f2.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+        // 创建 Niko 机器人实例
         Niko nikoBot = new Niko("Niko");
 
+        // 启动机器人
         nikoBot.start();
     }
 }
--- src/main/java/Ui.java
+++ src/main/java/Ui.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Ui {
-    private final Scanner scanner;
+    private Scanner scanner;
 
     public Ui() {
         this.scanner = new Scanner(System.in);
@@ -18,32 +18,9 @@ public class Ui {
         return scanner.nextLine();
     }
 
-    public void showAddTaskMessage(String task) {
+    public void showEcho(String message) {
         System.out.println("________________________________________");
-        System.out.println("added: " + task);
-        System.out.println("________________________________________");
-    }
-
-    public void showTaskList(Task[] tasks) {
-        System.out.println("________________________________________");
-        System.out.println("Here are the tasks in your list:");
-        for (int i = 0; i < tasks.length && tasks[i] != null; i++) {
-            System.out.println((i + 1) + ".[" + tasks[i].getStatusIcon() + "] " + tasks[i].getDescription());
-        }
-        System.out.println("________________________________________");
-    }
-
-    public void showMarkTaskMessage(Task task) {
-        System.out.println("________________________________________");
-        System.out.println("Nice! I've marked this task as done:");
-        System.out.println("  [" + task.getStatusIcon() + "] " + task.getDescription());
-        System.out.println("________________________________________");
-    }
-
-    public void showUnmarkTaskMessage(Task task) {
-        System.out.println("________________________________________");
-        System.out.println("OK, I've marked this task as not done yet:");
-        System.out.println("  [" + task.getStatusIcon() + "] " + task.getDescription());
+        System.out.println(message);
         System.out.println("________________________________________");
     }
 
