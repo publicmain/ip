c18b5b6bf62f7b90a95356f77bc61e5b1323cf56
publicmain <yaokexiang1107@163.com>
2024-08-29 12:17:34 +0800

    code standrad

--- src/main/java/Niko/Command/UnmarkCommand.java
+++ src/main/java/Niko/Command/UnmarkCommand.java
@@ -6,16 +6,16 @@ import Niko.Main.Ui;
 import Niko.Task.TaskList;
 
 public class UnmarkCommand extends Command {
-    private int index;
+    private int INDEX;
 
     public UnmarkCommand(int index) {
-        this.index = index;
+        this.INDEX = index;
     }
 
     @Override
     public void execute(TaskList tasks, Ui ui, Storage storage) throws NikoException {
-        tasks.unmarkTaskAsDone(index - 1);
-        ui.showUnmarkTaskMessage(tasks.getTask(index - 1));
+        tasks.unmarkTaskAsDone(INDEX - 1);
+        ui.showUnmarkTaskMessage(tasks.getTask(INDEX - 1));
         String readyToWrite = tasks.getTasks().toString();
         storage.write(readyToWrite.substring(1, readyToWrite.length() - 1));
     }
