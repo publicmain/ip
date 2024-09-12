59cf2246d08756b934c44c11a4bc416ce0786c85
publicmain <yaokexiang1107@163.com>
2024-09-12 14:50:23 +0800

    The getUserDialog method previously duplicated the logic of creating a Label and ImageView. This redundancy could lead to potential inconsistencies in future updates.
    This commit refactors getUserDialog to call getDukeDialog, eliminating code duplication and improving maintainability.
    
    Let's refactor the getUserDialog method by:
    * Reusing the getDukeDialog method to create the dialog box.
    * Reducing the redundancy of Label and ImageView instantiation in getUserDialog.
    
    This refactor simplifies the code and ensures consistent behavior between user and Duke dialogs.

--- src/main/java/niko/main/DialogBox.java
+++ src/main/java/niko/main/DialogBox.java
@@ -1,14 +1,11 @@
 package niko.main;
 
-import javafx.geometry.Pos;
+
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.scene.Node;
-import javafx.scene.layout.Region;
+
 
 public class DialogBox extends HBox {
 
@@ -16,22 +13,12 @@ public class DialogBox extends HBox {
         text.setWrapText(true);
         displayPicture.setFitWidth(100.0);
         displayPicture.setFitHeight(100.0);
-//        this.setAlignment(Pos.TOP_RIGHT);
 
         this.getChildren().addAll(displayPicture, text);
     }
-    private void flip() {
-        this.setAlignment(Pos.TOP_LEFT);
-        ObservableList<Node> tmp = FXCollections.observableArrayList(this.getChildren());
-        FXCollections.reverse(tmp);
-        this.getChildren().setAll(tmp);
-    }
-    public static DialogBox getUserDialog(String text, Image img) {
-        Label label = new Label(text);
-        ImageView imageView = new ImageView(img);
-        var db = new DialogBox(label,imageView);
 
-        return db;
+    public static DialogBox getUserDialog(String text, Image img) {
+        return getDukeDialog(text, img);
     }
 
     public static DialogBox getDukeDialog(String text, Image img) {
