3d723072ea6ae035708f9ec2337974046c5351eb
publicmain <yaokexiang1107@163.com>
2024-09-12 13:20:14 +0800

    Merge branch 'master' of https://github.com/publicmain/ip
    # Conflicts:
    #	example.txt
    #	src/main/java/niko/command/ListCommand.java

--- src/main/java/niko/main/Niko.java
+++ src/main/java/niko/main/Niko.java
@@ -37,7 +37,9 @@ public class Niko {
         storage = new Storage(filePath);
         try {
             ArrayList<Task> tasks = storage.load();
-            taskList.addTasks(tasks);
+            for (Task task : tasks) {
+                taskList.addTask(task);
+            }
             ui.showLoadSuccessMessage(tasks.size());
         } catch (IOException e) {
             throw new RuntimeException(e);
@@ -61,13 +63,19 @@ public class Niko {
      */
     public void processUserInput(String input) {
         try {
+            // Parse and execute the command
             Command command = Parser.parse(input);
             String response = command.execute(taskList, ui, storage);
+
+            // Show the user input and Niko's response in the MainWindow
             mainWindow.showDialog(input, response);
+
+            // Exit if the command is an exit command
             if (command.isExit()) {
                 ui.showGoodbyeMessage();
             }
         } catch (NikoException e) {
+            // Handle any Niko-specific exceptions and show the error message
             String response = ui.showErrorMessage(e.getMessage());
             mainWindow.showDialog(input, response);
         }
