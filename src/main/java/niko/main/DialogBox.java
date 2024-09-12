18775f9b6da74597be201db65d0fdaa52cc0d38e
publicmain <112534992+publicmain@users.noreply.github.com>
2024-09-12 13:37:26 +0800

    Update DialogBox.java

--- src/main/java/niko/main/DialogBox.java
+++ src/main/java/niko/main/DialogBox.java
@@ -1,54 +1,24 @@
 package niko.main;
 
-import java.io.IOException;
-import java.util.Collections;
-
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
-import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.Node;
+import javafx.scene.layout.Region;
 
-
-/**
- * Represents a dialog box consisting of an ImageView to represent the speaker's face
- * and a label containing text from the speaker.
- */
 public class DialogBox extends HBox {
-    @FXML
-    private Label dialog;
-    @FXML
-    private ImageView displayPicture;
 
-    private DialogBox(String text, Image img) {
-        try {
-            FXMLLoader fxmlLoader = new FXMLLoader(MainWindow.class.getResource("/view/DialogBox.fxml"));
-            fxmlLoader.setController(this);
-            fxmlLoader.setRoot(this);
-            fxmlLoader.load();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public DialogBox(Label text, ImageView displayPicture) {
+        text.setWrapText(true);
+        displayPicture.setFitWidth(100.0);
+        displayPicture.setFitHeight(100.0);
+//        this.setAlignment(Pos.TOP_RIGHT);
 
-
-        dialog.setText(text);
-        displayPicture.setImage(img);
-    }
-
-
-    /**
-     * Flips the dialog box such that the ImageView is on the left and text on the right.
-     */
-    private void flip() {
-        ObservableList<Node> tmp = FXCollections.observableArrayList(this.getChildren());
-        Collections.reverse(tmp);
-        getChildren().setAll(tmp);
-        setAlignment(Pos.TOP_LEFT);
+        this.getChildren().addAll(displayPicture, text);
     }
     private void flip() {
         this.setAlignment(Pos.TOP_LEFT);
@@ -57,14 +27,17 @@ public class DialogBox extends HBox {
         this.getChildren().setAll(tmp);
     }
     public static DialogBox getUserDialog(String text, Image img) {
+        Label label = new Label(text);
+        ImageView imageView = new ImageView(img);
+        var db = new DialogBox(label,imageView);
 
-        return new DialogBox(text, img);
+        return db;
     }
 
     public static DialogBox getDukeDialog(String text, Image img) {
-        var db = new DialogBox(text, img);
-        db.flip();
-        return db;
+        Label label = new Label(text);
+        ImageView imageView = new ImageView(img);
 
+        return new DialogBox(label,imageView);
     }
 }
